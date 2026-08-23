9bd4cd4e0abe415ef77adbec4eb3ef8da424e364
DineshSethu001 <dineshsethu15981@gmail.com>
2026-08-23 16:31:02 +0530

    ready

--- server/server.js
+++ server/server.js
@@ -194,6 +194,10 @@ app.put(
     });
   }
 });
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
