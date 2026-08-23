9fc21cab27617a9e05035f373c6c72ba10e31041
DineshSethu001 <dineshsethu15981@gmail.com>
2026-08-23 17:14:49 +0530

    Fix backend CORS

--- server/server.js
+++ server/server.js
@@ -17,8 +17,13 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "https://tech-care-project-2vrw.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);app.use(express.json());
 
 // Test route
 app.get("/", (req, res) => {
